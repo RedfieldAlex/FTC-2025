976a97230ca831977ad11475a18fd075797219a9
Alex Yu <alexyu810@gmail.com>
2025-07-23 20:25:37 -0400

    updated code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -70,7 +70,7 @@ public final class MecanumDrive {
         // feedforward parameters (in tick units)
         public double kS = 1.326242916918376;
         public double kV = 0.000273666375173515;
-        public double kA = 0;
+        public double kA = 0.00001; //NEED TO DOUBLE CHECK
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
